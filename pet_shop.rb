bb31d9d001fbc8c7a74bcffeb99376644aca726f
Paul <rpmstevenson@gmail.com>
2018-04-13 14:27:34 +0100

    pets sold

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,17 +1,30 @@
+#test1
 def pet_shop_name(shop)
   return shop[:name]
 end
 
+#test2
 def total_cash(cash)
    cash[:admin][:total_cash]
 end
 
+#Test3
+def add_or_remove_cash(total_cash, add_cash)
+  total_cash[:admin][:total_cash] -=(add_cash)
+end
 
-#Test 2
+#Test4
 def add_or_remove_cash(total_cash, add_cash)
   total_cash[:admin][:total_cash] +=(add_cash)
 end
-#Test 3
-def add_or_remove_cash(total_cash, add_cash)
-  total_cash[:admin][:total_cash] +=(add_cash)
+
+#test5
+def pets_sold(sold)
+  return sold[:admin][:pets_sold]
 end
+
+#test6
+def increase_pets_sold(total_sold, sold)
+  total_sold[:admin][:pets_sold] +=(sold)
+end
+#test7
