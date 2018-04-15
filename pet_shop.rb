45d036c2d8fae18042b36060a9f0702df8015143
Paul <rpmstevenson@gmail.com>
2018-04-15 08:26:05 +0100

    Tasks completed and ext attempted

--- pet_shop.rb
+++ pet_shop.rb
@@ -33,25 +33,71 @@ def stock_count(stock)
   return stock[:pets].count
 end
 
-#test8&9
+# test8&9
 def pets_by_breed(shop, breed)
-  i = 0
   total = []
   for pet in shop[:pets]
-    total.push(pet) if shop [:pets][i][:breed] == breed
-    i +=1
+    total.push(pet) if pet [:breed] == breed
   end
   return total
 end
 
-#test9 CANNOT DO
-# def find_pet_by_name(pets)
-#   for pet in pets[:pets][:name]
-#     return pet
-#   end
+def find_pet_by_name(shop, name)
+  for animal in shop[:pets]
+    return animal if animal[:name] == name
+  end
+  return nil
+end
+
+#test10
+def remove_pet_by_name(shop, name)
+  for animal in shop[:pets]
+    shop[:pets].delete(animal) if animal[:name] == name
+  end
+end
+
+#test11
+def add_pet_to_stock(shop, new_pet)
+  shop[:pets].push(new_pet)
+end
+# def stock_count(stock) ##counting stock
+#   return stock[:pets].count
 # end
 
+#test12
+def customer_cash(customer)
+  return customer[:cash]
+end
 
-# for chicken in chicken_hashes
-#   p "#{chicken [:name]} is #{chicken [:age]}"
-# return name[:pets][0][:name]
+#test13
+def remove_customer_cash(customer, amount)
+  return customer[:cash] -= amount
+end
+
+#test14
+def customer_pet_count(customer)
+  return customer[:pets].count
+end
+
+def add_pet_to_customer(customer, new_pet)
+  customer[:pets].push(new_pet)
+end
+
+### EXTRA
+def customer_can_afford_pet(customer, new_pet)
+  return true if customer[:cash] >= new_pet[:price]
+  return false if customer[:cash] < new_pet[:price]
+end
+
+## CALLING FROM TEST 8
+def sell_pet_to_customer(shop, pet, customer)
+
+
+customer_process = {}
+
+
+return shop[:admin[:pets_sold]
+return shop [:admin][:total_cash]
+return customer [:pets].count
+return customer [:cash]
+end
